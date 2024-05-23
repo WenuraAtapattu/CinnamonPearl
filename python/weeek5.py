476e02f618001319417f2054a0a1cf6857a7c8d9
WenuraAtapattu <wenuraatapattu41@gmail.com>
2024-05-23 13:44:57 +0530

    Delete python directory

--- python/weeek5.py
+++
@@ -1,9 +0,0 @@
-input_list = [10, 2, 7, 5, 38, 3]
-max_num = max(input_list)
-min_num = min(input_list)
-print("Maximum:", max_num)
-print("Minimum:", min_num)
-
-input_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-output_list = input_list[2:6]
-print(output_list)
