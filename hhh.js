46bfbb6163047dc9a7d6ebff2d1d3f9dd7a7ca8d
WenuraAtapattu <wenuraatapattu41@gmail.com>
2024-05-23 13:01:03 +0530

    update

--- hhh.js
+++
@@ -1,8 +0,0 @@
-function myFunction(){
-    document.getElementById('demo').innerHTML="Para Changed"
-}
-
-let myName= 'Natalia'
-let myCity= 'Kandy'
-
-console.log('My name is ${myName} and my city is ${myCity}');
